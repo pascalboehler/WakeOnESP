2564251452b1660d8eecb2290671215cdfeda152
Pascal Boehler <pascal@bembel.dev>
2023-03-06 14:45:42 +0100

    feat: Added functions to control power modes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
 
+int powerPin = 12;
+
 void setup() {
   // put your setup code here, to run once:
+  // TODO: Setup WiFi and Serial stuff
+
+  Serial.begin(112000);
+
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+}
+
+void powerOn() {
+  Serial.println("Turning PC on... one moment");
+}
+
+void powerOff() {
+  Serial.println("Turning PC off... one moment");
+}
+
+void longPress() {
+  Serial.println("Performing long press action");
+}
+
+void pressReset() {
+  Serial.println("Resetting your pc");
 }
