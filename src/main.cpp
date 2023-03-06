8f7ac4aa25a2f581c0304bd42b290a4c18ca7f58
Pascal Boehler <pascal@bembel.dev>
2023-03-06 15:58:22 +0100

    fix: Refactored code to fix issues

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,10 @@
 #include <Arduino.h>
 
+// Some global variables (i know, bad practice etc.)
+
 int powerPin = 12;
 
-void setup() {
-  // put your setup code here, to run once:
-  // TODO: Setup WiFi and Serial stuff
-
-  Serial.begin(112000);
-
-  
-}
-
-void loop() {
-
-}
+int powerLEDPin = 11;
 
 void powerOn() {
   Serial.println("Turning PC on... one moment");
@@ -30,3 +21,42 @@ void longPress() {
 void pressReset() {
   Serial.println("Resetting your pc");
 }
+
+bool isPoweredOn() {
+  if (digitalRead(powerLEDPin) == HIGH) {
+    return true;
+  }
+  return false;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  // TODO: Setup WiFi and Serial stuff
+
+  Serial.begin(112000);
+
+  Serial.println("Starting up system...");
+
+  Serial.println("Connecting to WiFi...");
+
+  Serial.println("Checking initial power state...");
+
+  if (isPoweredOn()) {
+    Serial.println("System already turned on");
+  } else {
+    Serial.println("System is not powered on");
+  }
+
+  Serial.println("Done.");
+}
+
+void loop() {
+  Serial.print("The current power state is: ");
+  if (isPoweredOn()) {
+    Serial.println("on");
+  } else {
+    Serial.println("off");
+  }
+  delay(5000);
+}
+
